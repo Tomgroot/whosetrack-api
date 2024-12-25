54b1fbcf7064b65efd9274ac3c02a831b2b7624a
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2024-12-25 12:25:42 +0100

    fix: mariadb aliases for delete queries

--- database/migrations/2024_12_26_221623_demo_on_pick_track.php
+++ database/migrations/2024_12_26_221623_demo_on_pick_track.php
@@ -15,9 +15,9 @@ return new class extends Migration
             FOR EACH ROW
             BEGIN
                 IF NEW.status = 'pick_track' AND OLD.status != 'pick_track' AND (NEW.id = 1 OR NEW.id = 2) THEN
-                    DELETE FROM guesses g WHERE g.track_id IN (SELECT t.id FROM tracks t WHERE t.round_id = NEW.id AND t.user_id = 1);
-                    DELETE FROM guesses g WHERE g.user_id = 1 AND g.track_id IN (SELECT t.id FROM tracks t WHERE t.round_id = NEW.id);
-                    DELETE FROM tracks t WHERE t.round_id = NEW.id AND t.user_id = 1;
+                    DELETE FROM guesses WHERE track_id IN (SELECT t.id FROM tracks t WHERE t.round_id = NEW.id AND t.user_id = 1);
+                    DELETE FROM guesses WHERE user_id = 1 AND track_id IN (SELECT t.id FROM tracks t WHERE t.round_id = NEW.id);
+                    DELETE FROM tracks WHERE round_id = NEW.id AND user_id = 1;
                 END IF;
             END
         EO);
