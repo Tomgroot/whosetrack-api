e3fdf3b5566609ad0fe630c6e58fd79cf8197752
Tom Groot <4732713+Tomgroot@users.noreply.github.com>
2024-12-31 00:23:17 +0100

    fix: eager loading users for faster query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void {
         $this->app->usePublicPath(__DIR__.'/../../public_html');
+
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
